e90a1d7a1477b76b247a7db2a777e95e810d16ce
jonbezer <jbbarroz@gmail.com>
2026-09-21 18:57:32 -0300

    module01

--- py_module_01/ex1/ft_garden_data.py
+++ py_module_01/ex1/ft_garden_data.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
-class 
+class Plant:
+    pass
+
+
+if __name__ == "__main__":
+    rose = Plant()
+    sunflower = Plant()
+    cactus = Plant()
+
+    rose.name = "Rose"
+    rose.height = 25
+    rose.age = 30
+
+    sunflower.name = "Sunflower"
+    sunflower.height = 80
+    sunflower.age = 45
+
+    cactus.name = "Cactus"
+    cactus.height = 15
+    cactus.age = 120
